c72533f522fac0de8b07c3f42b1c5b016ab7114c
jivoy1988 <jivoy1988@gmail.com>
2018-07-02 18:44:32 +0500

    Add Unit fixtures

--- src/CompoEcommerceDemo/AppBundle/DataFixtures/LoadUnitData.php
+++ src/CompoEcommerceDemo/AppBundle/DataFixtures/LoadUnitData.php
@@ -11,6 +11,7 @@
 
 namespace CompoEcommerceDemo\AppBundle\DataFixtures;
 
+use Compo\UnitBundle\Entity\Unit;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -62,5 +63,86 @@ class LoadUnitData extends AbstractFixture implements ContainerAwareInterface, O
      */
     public function load(ObjectManager $manager)
     {
+        $data = [
+            [
+                'name' => 'Штука',
+                'shortNameNational' => 'шт',
+                'literalNameNational' => 'ШТ',
+                'shortNameInternational' => 'pc',
+                'literalNameInternational' => 'PCE',
+                'type' => 'integer'
+            ],
+            [
+                'name' => 'Упаковка',
+                'shortNameNational' => 'упак',
+                'literalNameNational' => 'УПАК',
+                'shortNameInternational' => 'pkg',
+                'literalNameInternational' => 'NMP',
+                'type' => 'integer'
+            ],
+            [
+                'name' => 'Сантиметр',
+                'shortNameNational' => 'см',
+                'literalNameNational' => 'СМ',
+                'shortNameInternational' => 'cm',
+                'literalNameInternational' => 'CMT',
+                'type' => 'decimal'
+            ],
+            [
+                'name' => 'Миллиметр',
+                'shortNameNational' => 'мм',
+                'literalNameNational' => 'ММ',
+                'shortNameInternational' => 'mm',
+                'literalNameInternational' => 'MMT',
+                'type' => 'decimal'
+            ],
+            [
+                'name' => 'Метр',
+                'shortNameNational' => 'м',
+                'literalNameNational' => 'М',
+                'shortNameInternational' => 'm',
+                'literalNameInternational' => 'MTR',
+                'type' => 'decimal'
+            ],
+            [
+                'name' => 'Комплект',
+                'shortNameNational' => 'компл',
+                'literalNameNational' => 'КОМПЛ',
+                'shortNameInternational' => 'set',
+                'literalNameInternational' => 'SET',
+                'type' => 'integer'
+            ],
+            [
+                'name' => 'Килограмм',
+                'shortNameNational' => 'кг',
+                'literalNameNational' => 'КГ',
+                'shortNameInternational' => 'kg',
+                'literalNameInternational' => 'KGM',
+                'type' => 'decimal'
+            ],
+            [
+                'name' => 'Квадратный метр',
+                'shortNameNational' => 'м<sup>2</sup>',
+                'literalNameNational' => 'М2',
+                'shortNameInternational' => 'm2',
+                'literalNameInternational' => 'MTK',
+                'type' => 'decimal'
+            ],
+        ];
+
+        foreach ($data as $item) {
+            $newItem = new Unit();
+
+            $newItem->setName($item['name']);
+            $newItem->setShortNameNational($item['shortNameNational']);
+            $newItem->setLiteralNameNational($item['literalNameNational']);
+            $newItem->setShortNameInternational($item['shortNameInternational']);
+            $newItem->setLiteralNameInternational($item['literalNameInternational']);
+            $newItem->setType($item['type']);
+
+            $manager->persist($newItem);
+        }
+
+        $manager->flush();
     }
 }
