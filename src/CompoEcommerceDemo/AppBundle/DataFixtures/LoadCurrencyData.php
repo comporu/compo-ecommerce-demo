567719536fbf5307e0bae5dc1abade089a63e628
jivoy1988 <jivoy1988@gmail.com>
2018-07-02 18:28:02 +0500

    Add Currency fixtures

--- src/CompoEcommerceDemo/AppBundle/DataFixtures/LoadCurrencyData.php
+++ src/CompoEcommerceDemo/AppBundle/DataFixtures/LoadCurrencyData.php
@@ -11,6 +11,7 @@
 
 namespace CompoEcommerceDemo\AppBundle\DataFixtures;
 
+use Compo\CurrencyBundle\Entity\Currency;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -62,5 +63,46 @@ class LoadCurrencyData extends AbstractFixture implements ContainerAwareInterfac
      */
     public function load(ObjectManager $manager)
     {
+        $data = [
+            [
+                'name' => 'Российский убль',
+                'code' => 'RUB',
+                'rate' => 1,
+                'sign' => 'руб.',
+                'symbol' => 'руб.',
+                'cbrf' => false
+            ],
+            [
+                'name' => 'Евро',
+                'code' => 'EUR',
+                'rate' => 70,
+                'sign' => 'евро',
+                'symbol' => 'евро',
+                'cbrf' => true
+            ],
+            [
+                'name' => 'Доллар',
+                'code' => 'USD',
+                'rate' => 60,
+                'sign' => 'доллар',
+                'symbol' => '$',
+                'cbrf' => true
+            ],
+        ];
+
+        foreach ($data as $item) {
+            $newItem = new Currency();
+
+            $newItem->setName($item['name']);
+            $newItem->setCode($item['code']);
+            $newItem->setRate($item['rate']);
+            $newItem->setSign($item['sign']);
+            $newItem->setSymbol($item['symbol']);
+            $newItem->setUpdateCbrf($item['cbrf']);
+
+            $manager->persist($newItem);
+        }
+
+        $manager->flush();
     }
 }
